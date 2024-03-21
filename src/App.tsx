39c21c2b62aab547b15351fd84d283ac920c9271
avishaikkumar09 <avishaikkumar09@gmail.com>
2024-03-21 14:18:27 +0530

    added frontend with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+<h1>Hello people</h1>
+
+<Button variant={"destructive"}>click me </Button>
+    </>
+  )
+}
+
+export default App
